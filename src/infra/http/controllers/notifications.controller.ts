0c278a6bf46ef2be937ee63c31155070cd04c1a0
Felipe Souza <felipelsouza.dev@gmail.com>
2022-12-19 00:00:12 -0300

    Feature: Infra and Dependency Injection

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,10 +1,56 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { SendNotification } from 'src/application/use-cases/send-notification';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-view-model';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+    private countRecipientNotifications: CountRecipientNotifications,
+    private getRecipientNotifications: GetRecipientNotifications,
+    private readNotification: ReadNotification,
+    private unreadNotification: UnreadNotification
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') notificationId: string) {
+    await this.cancelNotification.execute({ notificationId });
+  }
+
+  @Get('from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications } = await this.getRecipientNotifications.execute({
+      recipientId
+    });
+
+    return { notifications: notifications.map(NotificationViewModel.toHttp) };
+  }
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    const { count } = await this.countRecipientNotifications.execute({
+      recipientId
+    });
+
+    return { count };
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') notificationId: string) {
+    await this.readNotification.execute({ notificationId });
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') notificationId: string) {
+    await this.unreadNotification.execute({ notificationId });
+  }
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
@@ -16,6 +62,8 @@ export class NotificationsController {
       recipientId
     });
 
-    return notification;
+    return {
+      notification: NotificationViewModel.toHttp(notification)
+    };
   }
 }
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,12 +1,42 @@
-import { Notification } from 'src/application/entities/notification';
-import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+import { Notification } from '@application/entities/notification';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
 
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (notification) => notification.id === notificationId
+    );
+
+    return notification || null;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId
+    );
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId
+    ).length;
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (persistedNotification) => persistedNotification.id === notification.id
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
   }
 }
